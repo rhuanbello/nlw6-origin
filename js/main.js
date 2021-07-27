e0fd07fc9935953086a1ef6a99c06676b2a30a56
rhuanbello <rhuanbello@gmail.com>
2021-07-27 16:14:02 -0300

    Add Testimonials + Contact + Swiper.js + ScrollReveal

--- js/main.js
+++ js/main.js
@@ -31,3 +31,34 @@ window.addEventListener('scroll', function() {
         header.classList.remove('scroll')
     }
 })
+
+// Slider
+
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    pagination: {
+        el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true,
+
+});
+
+// Scroll Reveal
+
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: true
+})
+
+scrollReveal.reveal(`
+    #home .text, #home .image,
+    #about .image, #about .text,
+    #services header, #services .card,
+    #testimonials header, #testimonials .testimonials
+    #contact .text, #contact .links
+    `,
+    { interval: 100}
+)
