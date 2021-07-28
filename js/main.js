b37a66ebbf0614f5eb7c4b7c866be11b5e59a12c
rhuanbello <rhuanbello@gmail.com>
2021-07-28 18:54:48 -0300

    Final V1.00

--- js/main.js
+++ js/main.js
@@ -21,9 +21,10 @@ for (const link of links) {
 
 // Adicionar sombra no header ao scrollar página
 
+const header = document.querySelector("#header")
+const navHeight = header.offsetHeight
+
 function changeHeader() {
-    const header = document.querySelector("#header")
-    const navHeight = header.offsetHeight
 
     if(window.scrollY >= navHeight) {
         header.classList.add('scroll')
@@ -41,7 +42,12 @@ const swiper = new Swiper('.swiper-container', {
     },
     mousewheel: true,
     keyboard: true,
-
+    breakpoints: {
+        767: {
+            slidesPerView: 2,
+            setWrapperSize: true
+        }
+    }
 });
 
 // Scroll Reveal
@@ -66,10 +72,11 @@ scrollReveal.reveal(`
 
 // Back to Top
 
+const backToTop = document.querySelector('a.back-to-top')
+const about = document.querySelector("#about")
+const aboutHeight = about.offsetHeight
+
 function backToTheTop () {
-    const backToTop = document.querySelector('a.back-to-top')
-    const about = document.querySelector("#about")
-    const aboutHeight = about.offsetHeight
 
     if(window.scrollY >= aboutHeight) {
         backToTop.classList.add('show')
@@ -78,7 +85,36 @@ function backToTheTop () {
     }
 }
 
+// Menu ativo conforme ativo na página
+
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+
+    const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+    for ( const section of sections ) {
+        const sectionTop = section.offsetTop
+        const sectionHeight = section.offsetHeight
+        const sectionId = section.getAttribute('id')
+
+        const checkpointStart = checkpoint >= sectionTop
+        const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+        if (checkpointStart && checkpointEnd) {
+            document.querySelector('nav ul li a[href*=' + sectionId + ']' )
+            .classList.add('active')
+        } else {
+            document.querySelector('nav ul li a[href*=' + sectionId + ']' )
+            .classList.remove('active')
+        }
+    }
+
+}
+
+// Ao Scrollar
+
 window.addEventListener('scroll', function() {
     changeHeader()
     backToTheTop()
+    activateMenuAtCurrentSection()
 })
